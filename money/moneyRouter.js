043c9c1bfe1904aaebb729d8c6452cef2a4bcc07
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-09 13:07:57 -0800

    Create GET for global of accounts table.

--- money/moneyRouter.js
+++ money/moneyRouter.js
@@ -4,16 +4,16 @@ const knex = require('../data/dbConfig');
 
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     knex.select('*')
-//     .from("budget")
-//     .then(posts => {
-//       res.status(200).json(posts);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       res.status(500).json({ errorMessage: "Error getting the data" });
-//     });
-// });
+router.get('/', (req, res) => {
+    knex.select('*')
+    .from("accounts")
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "Error getting the data" });
+    });
+});
 
 module.exports = router;
