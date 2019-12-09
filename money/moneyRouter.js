1173940bf71ccf81168b9e1ae09212d96af22d48
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-09 13:57:32 -0800

    Create POST to db.

--- money/moneyRouter.js
+++ money/moneyRouter.js
@@ -5,7 +5,8 @@ const knex = require('../data/dbConfig');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    knex.select('*')
+  knex
+    .select('*')
     .from("accounts")
     .then(posts => {
       res.status(200).json(posts);
@@ -15,5 +16,40 @@ router.get('/', (req, res) => {
       res.status(500).json({ errorMessage: "Error getting the data" });
     });
 });
+
+router.post('/', validatePostInfo, (req, res) => {
+  const postInfo = req.body
+  knex("accounts")
+    .insert(postInfo)
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "Error adding the post", error });
+    });
+});
+
+
+
+function validatePostInfo(req, res, next) {
+  const postInfo = req.body
+
+  if (!postInfo) {
+    res.status(404).json({ errorMessage: "Missing Necessary information" })
+  } else {
+    next();
+  }
+}
+
+function ValidateId(req, res, next) {
+  const id = req.params.id
+
+  if (!id) {
+    res.status(400).json({ message: "Wrong Id" })
+  } else {
+    next();
+  }
+}
+
 
 module.exports = router;
